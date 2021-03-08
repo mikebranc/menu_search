ac7567b9594fcb480bc3ee6561fe4c006766ca66
Michael Branconier <50855884+mikebranc@users.noreply.github.com>
2021-03-07 19:05:46 -0800

    Updated Starter Code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useEffect, useState} from 'react'
+import {Container, Row, Col} from 'react-bootstrap'
+import PostForm from './PostForm'
+import PostList from './PostList'
+import PostModal from './PostModal'
+ 
 function App() {
+  const [posts, setPosts] = useState([])
+  const[show, setShow] = useState(false)
+  const[postId, setPostId] = useState('')
+  const[loading, setLoading] = useState(false)
+
+  useEffect (() =>{
+    setLoading(true)
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then((json) => {
+      setPosts(json)
+      setLoading(false)
+    })
+  }, [setLoading, setPosts])
+  function addPostToList(newPost){
+    setPosts([newPost, ...posts])
+  }
+  function deletePost(postId){
+    setPosts((prevPosts) => prevPosts.filter((post)=> post.id !== postId))
+  }
+  function displayPostInModal(postId){
+    setPostId(postId)
+    setShow(true) 
+  }
+  function closePostModal(){
+    setShow(false)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+    <Row className="justify-content-md-center" style={{marginTop: 60 }}>
+    <Col xs lg="12">
+      <h1 style={{textAlign: 'center'}}>Blog Posts</h1>
+    </Col>
+    </Row>
+    <Row className="justify-content-md-center" style={{marginTop: 20   }}>
+      <Col xs lg="12">
+        <PostForm addPostToList = {addPostToList} />
+      </Col>
+    </Row>
+    <Row className="justify-content-md-center" style={{marginTop: 20 }}>
+      <Col xs lg="12">
+        <PostList posts={posts} deletePost = {deletePost} displayPostInModal = {displayPostInModal}/>
+      </Col>
+    </Row>
+    <PostModal closePostModal = {closePostModal} show={show} postId = {postId}/>
+</Container>
   );
 }
 
---
+++ src/PostForm.jsx
@@ -0,0 +1,47 @@
+import React, {useState} from 'react'
+import {Form, Button, Card} from 'react-bootstrap'
+
+ export default function (props){
+    const { addPostToList } = props
+    const [checked, setChecked] = useState(false)
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
+    function handleSubmit(event){
+        event.preventDefault()
+        addPostToList({ id: String(Math.random()*1000 + 100), title, body })
+        setTitle('')
+        setBody('')
+        setChecked(false)
+    }
+    return (
+        <Form onSubmit = {handleSubmit}>
+            <Form.Check 
+                type='checkbox' 
+                label='Add Post' 
+                checked={checked} 
+                onChange = {() => setChecked((prevState) => !prevState)}/>
+            {checked && (
+                <>
+                <Form.Group>
+                <Form.Control 
+                    type='input' 
+                    placeholder='Title' 
+                    value={title}
+                    onChange= {(event) => setTitle(event.target.value)}/>
+            </Form.Group>
+                <Form.Control 
+                    as='textarea' 
+                    rows={10} 
+                    value={body}
+                    onChange= {(event) => setBody(event.target.value)}/>
+            <Form.Group>
+
+            </Form.Group>
+            <Button variant='primary' type='submit'> Submit</Button>
+            </>
+            )}
+            <hr></hr>
+        </Form>
+    )
+
+ }
