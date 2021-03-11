ea07cd6b379ab8d844d2cc27c3f045e636d61455
Brady Amundson <brady.amundson98@gmai.com>
2021-03-11 15:27:26 -0600

    working on button

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,15 +1,24 @@
 import React, {useState, useEfect} from 'react' 
 import {Form, Button, Card} from 'react-bootstrap'
 import {useHistory} from 'react-router-dom'
+import { matchPath } from "react-router"
 
    
 
 export default function SearchBar(props){
-
+   
+      
     const {getTitle} = props
     const [title, setTitle] = useState('')
     let history = useHistory()
-    console.log("history: ", history)
+    const query = matchPath(history.location.pathname,{
+        path: '/results',
+        exact: true,
+        strict: false
+      })
+    //   console.log(query.isExact)
+
+
     function handleSubmit(event){
         event.preventDefault()
         history.push(`/results?query=${title}`)
@@ -18,7 +27,7 @@ export default function SearchBar(props){
     
     function handleChange(event){
         setTitle(event.target.value)
-        getTitle(event.target.value)
+        //getTitle(event.target.value)
     }
 
 return(
@@ -26,18 +35,19 @@ return(
     {(
         <>
         <Form.Group>
-        <Form.Control 
-            style = {{fontSize:30, padding:10, borderRadius:20 }}
-            type='input' 
-            placeholder='🔍 Search from over 115,000 menu items' 
-            value={title}
-            onChange = {handleChange}/>
-    </Form.Group>
-    </>
-    )}
-    <hr></hr>
-    
-</Form>
+            <Form.Control 
+                style = {{fontSize:30, padding:10, borderRadius:20 }}
+                type='input' 
+                placeholder='🔍 Search from over 115,000 menu items' 
+                value={title}
+                onChange = {handleChange}/>
+        </Form.Group>
+            <button onClick={handleSubmit} style={{display: query.isExact ? 'null' : 'none', marginTop: 80, borderRadius: 20, border: 0, backgroundColor: "#EBD489", color:'black', fontWeight:'bold', width: '250px', height: '70px', fontSize: 30}}>Search</button> 
+        </>
+    )}  
+    </Form>
 )
 }
 // query ? null :  <button onClick={handleSubmit}>submit</button> 
+
+//<hr></hr>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,7 +5,7 @@ import PostModal from '../Components/PostModal'
 import NavBar from '../Components/NavBar'
 import {Container, Row, Col, Button} from 'react-bootstrap'
 import SearchBar from '../Components/SearchBar'
-//import bgPic from '../Images/eggs-toast.jpg'
+import bgPic from '../Images/eggs-toast.jpg'
 import {useHistory} from 'react-router-dom'
 
 
@@ -25,10 +25,10 @@ export default function Home(){
   //backgroundImage:`url(${bgPic})`
 
     return(
-        <div style={{fontFamily: 'Rammetto One', height:'100vh', backgroundPosition:'center', backgroundSize: 'cover'}}>
+        <div style={{fontFamily: 'Rammetto One', height:'100vh', backgroundPosition:'center', backgroundImage:`url(${bgPic})`, backgroundSize: 'cover'}}>
         <div style={{backgroundColor: 'rgba(176, 186, 118, 0.3)', height:'100%'}}>
             <NavBar />
-            <div style={{opacity: '1',display: 'flex', alignItems: 'center',  paddingTop: 200, flexDirection: 'column', width:'100%'}}>
+            <div style={{opacity: '1', display: 'flex', alignItems: 'center',  paddingTop: 200, flexDirection: 'column', width:'100%'}}>
               <div style={{width: '60%' }}>
                 <SearchBar />
               </div>
