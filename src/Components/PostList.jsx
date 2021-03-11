855542785a06f8dc800fb806fa70095cda2c8d39
Brady Amundson <brady.amundson98@gmai.com>
2021-03-11 14:33:14 -0600

    Card styling

--- src/Components/PostList.jsx
+++ src/Components/PostList.jsx
@@ -8,13 +8,12 @@ export default function PostList(props){
             <CardColumns>
                 {posts && 
                     posts.map((post) =>(
-                    <Card key ={post.id} className="text-center" style={{height: '400px', cursor: 'pointer'}} >
+                    <Card key ={post.id} className="text-center" style={{height: '400px', cursor: 'pointer', backgroundColor: 'rgba(176, 186, 118, 0.3)', borderRadius:20}} >
                         <Card.Body onClick = {() => displayPostInModal(post.id)}>
                             {/* <Card.Img variant="top" src={post.image}/> */}
-                            <Card.Img variant='top' style={{height:'250px', width:'300px', objectFit: 'cover'}} src='https://images.spoonacular.com/file/wximages/424571-636x393.png'/>
-                            <Card.Title style={{fontWeight: 'bold', textAllign: 'center'}}>{post.title}</Card.Title>
-                            <Card.Body>In-N-Out</Card.Body>
-                            <Card.Text>{post.restaurantChain}</Card.Text>
+                            <Card.Img variant='top' style={{height:'250px', width:'300px', objectFit: 'cover', borderRadius:20}} src='https://images.spoonacular.com/file/wximages/419357-312x231.png'/>
+                            <Card.Title style={{fontWeight: 'bold', textAllign: 'center', fontSize:'200%' }}>{`${post.title.slice(0,25)}...`}</Card.Title>
+                            <Card.Text style={{fontSize: '150%'}}>{post.restaurantChain}In-b-out</Card.Text>
                         </Card.Body>
                     </Card>
                     ))}
